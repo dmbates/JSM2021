6139b59c58ccb1d5a288b96d8bcc66eedaf65fe6
Douglas Bates <dmbates@gmail.com>
2021-07-09 14:24:09 -0500

    Initial notebook to show Cecile and Claudia

---
+++ notebooks/intro.jl
@@ -0,0 +1,62 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 8b92a6cf-cd6a-4364-9f59-598bcf206ef1
+html"<button onclick=present()>Present</button>"
+
+# ╔═╡ 5c402564-e0e1-11eb-105b-951cfa19cacd
+md"""
+# Julia for Statistics and Data Science
+
+- *Cecile Ane*, *Douglas Bates* & *Claudia Solis-Lemus* all with U. of Wisconsin - Madison
+
+1. What is **Julia**?
+    - How does it differ from other languages, particularly **R** and **Python**, used for data science?
+    - How do I get started?
+
+2. Motivating example -- `MixedModels.jl` compared to R's `lme4`
+
+3. Packages for Data Science
+
+4. Packages for Statistics
+"""
+
+# ╔═╡ 7e8e3903-a76d-4310-b5df-8508fdd7dba3
+md"""
+## What is **Julia**?
+
+- an Open Source language designed for "technical computing"
+- development of the [language itself](https://github.com/JuliaLang/julia) and most packages, e.g. [DataFrames](https://github.com/JuliaData/DataFrames.jl), takes place on github.
+- a few things to notice about [github.com/JuliaLang/julia](https://github.com/JuliaLang/julia)
+    - nearly 1200 contributors
+    - over 40,000 issues or pull requests
+    - nearly 50,000 commits
+    - the most common coding language used in the `julia` repository is Julia
+
+- installation is easiest using binary downloads from [julialang.org](https://julialang.org/downloads)
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.0-beta3"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─8b92a6cf-cd6a-4364-9f59-598bcf206ef1
+# ╟─5c402564-e0e1-11eb-105b-951cfa19cacd
+# ╟─7e8e3903-a76d-4310-b5df-8508fdd7dba3
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
